2735c2a45998ee3ea642bdf4001e1026d2d4f16c
RosaCasa <rosamari.sab@gmail.com>
2020-11-03 18:32:18 +0100

    started home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Containers/Home/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <BrowserRouter>
+
+      <Header />
+
+      <Route path='/' exact>
+        <Home>
+
+        </Home>
+      </Route>
+
+    </BrowserRouter>
   );
 }
 
