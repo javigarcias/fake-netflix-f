69f43599b23ef467aa776d763173e92ab48468ed
RosaSabater <rosamari_sab@hotmail.com>
2020-11-10 20:54:03 +0100

    fixes admin design

--- src/Containers/Admin/Admin.jsx
+++ src/Containers/Admin/Admin.jsx
@@ -36,47 +36,51 @@ export default function AdminOrders() {
     return (
 
         <>
-            <div className="containerCards">
-                {orders.map(order => {
+            <div className="padreContainer">
 
-                    return (
+                <div className="containerCards">
+
+                    {orders.map(order => {
+
+                        return (
 
 
-                        <div className="card">
+                            <div className="card">
 
-                            <img className="moviePoster" src={`https://image.tmdb.org/t/p/w200/${order.movieId.poster_path}`} alt="poster"></img>
+                                <img className="moviePoster" src={`https://image.tmdb.org/t/p/w200/${order.movieId.poster_path}`} alt="poster"></img>
 
-                            <div className="texto">
-                                <p>
-                                    {order.movieId.title}
-                                </p>
+                                <div className="texto">
+                                    <p>
+                                        {order.movieId.title}
+                                    </p>
 
-                                <p>
-                                    {`${order.rentalState ? 'Alquilada' : 'Otro'}`}
-                                </p>
+                                    <p>
+                                        {`${order.rentalState ? 'Alquilada' : 'Otro'}`}
+                                    </p>
 
-                                <p>
-                                    Inicio alquiler: {dayjs(order.rentalDate).format('DD-MM-YYYY')}
-                                </p>
+                                    <p>
+                                        Inicio alquiler: {dayjs(order.rentalDate).format('DD-MM-YYYY')}
+                                    </p>
 
-                                <p>
-                                    Fin alquiler: {dayjs(order.rentalEndDate).format('DD-MM-YYYY')}
-                                </p>
+                                    <p>
+                                        Fin alquiler: {dayjs(order.rentalEndDate).format('DD-MM-YYYY')}
+                                    </p>
 
-                                <p>
-                                    Alquilada por: {order.userId?.name} {order.userId?.surname}
-                                </p>
+                                    <p>
+                                        Alquilada por: {order.userId?.name} {order.userId?.surname}
+                                    </p>
 
-                                <p>
-                                    Fin alquiler: {dayjs(order.rentalEndDate).format('DD-MM-YYYY')}
-                                </p>
+                                    <p>
+                                        Fin alquiler: {dayjs(order.rentalEndDate).format('DD-MM-YYYY')}
+                                    </p>
+                                </div>
                             </div>
-                        </div>
 
 
-                    )
-                }
-                )}
+                        )
+                    }
+                    )}
+                </div>
             </div>
         </>
 
