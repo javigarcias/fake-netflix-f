b514590f9ad719ebd62ae6c4c835585d756d6e7e
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-11-10 10:50:10 +0100

    Starting feature Homepage

---
+++ src/Containers/Homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+import '.Homepage.scss';
+
+const Homepage = () => {
+
+    return (
+        <div><h3>Homepage</h3></div>
+    )
+}
+
+export default Homepage;
+
