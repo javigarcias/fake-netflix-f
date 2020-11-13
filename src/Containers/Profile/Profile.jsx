a031fc5593c54dc45e016799f951bfa7bb072853
javigarcia <javigarciaweb@gmail.com>
2020-11-13 17:27:52 +0100

    Change className Profile in profile container

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -1,37 +1,40 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
-import {connect} from 'react-redux';
-import {USER_ORDERS} from '../../Redux/types';
-
+import { connect } from 'react-redux';
+import { USER_ORDERS } from '../../Redux/types';
 
 import './Profile.scss';
 
-const Profile = ({dispatch, user, orders}) => {
-    
+const Profile = ({ dispatch, user, orders }) => {
+
     useEffect(() => {
         const options = {
             headers: { Authorization: `Bearer ${user.token}` },
-          };
-        axios.get (`${process.env.REACT_APP_APIURL}/order/show`, options)
-        .then (orders => dispatch({ type: USER_ORDERS, payload: orders.data}))
-        .catch (error => console.log())
-       
+        };
+        axios.get(`${process.env.REACT_APP_APIURL}/order/show`, options)
+            .then(orders => dispatch({ type: USER_ORDERS, payload: orders.data }))
+            .catch(error => console.log())
+
     }, [])
-    
+
     return (
-        <div className='profile'>
-           
-          {orders?.map(order =>
-            <div className="order" key={order._id}>
-                <h3>{order.movieId.title}</h3>
-                <img src={'https://image.tmdb.org/t/p/w500' + order.movieId.poster_path} />
-                <h4>Rental date: {order.rentalDate}</h4>
-                <h4>Rental end date: {order.rentalEndDate}</h4>
+        <div className='profileView'>
+
+            {orders?.map(order =>
+                <div className="cardOrder" key={order._id}>
+                    <div className="titleCard">{order.movieId.title}</div>                <h2>{order.movieId.title}</h2>
+                    <div className="imageCard">
+                        <img src={'https://image.tmdb.org/t/p/w500' + order.movieId.poster_path} />
+                    </div>
+                    <div className="infoCard">
+                        FECHA ALQUILER: {order.rentalDate}
+                        FECHA DEVOLUCIÓN: {order.rentalEndDate}
+                    </div>
 
 
-            </div>)}         
-        
-        
+                </div>)}
+
+
         </div>
     )
 }
@@ -39,7 +42,7 @@ const mapStateToProps = state => {
     console.log(state.user)
     return {
         user: state.user,
-        orders:state.orders
+        orders: state.orders
     }
 }
 
