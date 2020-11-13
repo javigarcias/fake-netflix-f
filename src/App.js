2cfcf35b3a644f922604d688178dc7ae2a38ccf4
RosaSabater <rosamari_sab@hotmail.com>
2020-11-13 20:18:24 +0100

    vistas header

--- src/App.js
+++ src/App.js
@@ -14,6 +14,18 @@ import DramaMoviesCarousel from './Components/Carrousels/DramaMovies/DramaMovies
 import HorrorMoviesCarousel from './Components/Carrousels/HorrorMovies/HorrorMoviesCarousel'
 import PopularMoviesCarousel from './Components/Carrousels/PopularMovies/PopularMoviesCarousel'
 import KidsMoviesCarousel from './Components/Carrousels/KidsMovies/KidsMoviesCarousel'
+import Terror from './Containers/Terror/Terror';
+import Clasicos from './Containers/Clasicos/Clasicos';
+import Drama from './Containers/Drama/Drama';
+import Documental from './Containers/Documental/Documental';
+import Niños from './Containers/Niños/Niños';
+import Comedia from './Containers/Comedia/Comedia';
+import Accion from './Containers/Accion/Accion';
+import Misterio from './Containers/Misterio/Misterio';
+import Recientes from './Containers/Recientes/Recientes';
+import Peliculas from './Containers/Peliculas/Peliculas';
+import Populares from './Containers/Populares/Populares';
+import Estrenos from './Containers/Estrenos/Estrenos';
 
 
 
@@ -56,6 +68,65 @@ function App() {
                 <Profile />
             </Route>
 
+            <Route path='/peliculas' exact>
+                <Header />
+                <Peliculas />
+            </Route>
+
+            <Route path='/populares' exact>
+                <Header />
+                <Populares />
+            </Route>
+
+            <Route path='/estrenos' exact>
+                <Header />
+                <Estrenos />
+            </Route>
+
+            <Route path='/recientes' exact>
+                <Header />
+                <Recientes />
+            </Route>
+
+            <Route path='/terror' exact>
+                <Header />
+                <Terror />
+            </Route>
+
+            <Route path='/clasicos' exact>
+                <Header />
+                <Clasicos />
+            </Route>
+
+            <Route path='/drama' exact>
+                <Header />
+                <Drama />
+            </Route>
+
+            <Route path='/documental' exact>
+                <Header />
+                <Documental />
+            </Route>
+
+            <Route path='/niños' exact>
+                <Header />
+                <Niños />
+            </Route>
+
+            <Route path='/comedia' exact>
+                <Header />
+                <Comedia />
+            </Route>
+
+            <Route path='/accion' exact>
+                <Header />
+                <Accion />
+            </Route>
+
+            <Route path='/misterio' exact>
+                <Header />
+                <Misterio />
+            </Route>
         </BrowserRouter>
     );
 }
