1bf179a91518ea78025b43263cf4ff58f1347649
RosaSabater <rosamari_sab@hotmail.com>
2020-11-15 11:30:18 +0100

    fix carousel

--- src/Components/MostrarCarrousel/MostrarCarrousel.jsx
+++ src/Components/MostrarCarrousel/MostrarCarrousel.jsx
@@ -34,49 +34,46 @@ export default function MostrarCarrousel({ endpoint }) {
     const [showMovieActual, setShowMovieActual] = useState({})
 
     useEffect(() => {
-        
+
         const fnc = async () => {
             try {
-                
+
                 let respuesta = await axios.get(endpoint);
-                
+
                 setMovies(respuesta.data);
-                
+
             } catch (error) {
                 console.log(error);
             }
         }
         fnc();
-    },[]);
-    
-    
+    }, []);
+
+
     const rent = async (movie) => {
         const userLog = JSON.parse(localStorage.getItem('usuario'));
         console.log(userLog)
         let orderBody = {
             userId: userLog.userId,
-            movieId: movie.id           
+            movieId: movie.id
         };
         await axios.post('http://localhost:3000/order/rent', orderBody)
     };
-
-    }
-
 
     return (
         <>
             <Slider {...config}>
                 {movies?.map(movie => {
-                
+    
                     return <div className="wrapper"><div key={movie._id} className="img-card">
-
+    
                         <div className="imghover"> <img className="img" id="img" src={!movie.poster_path ? 'Images/notfound.png' : `https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt='poster_path' />
-
+    
                             <div className='movie-card' id="movie-card">
                                 <div className='title'><p><strong>{movie.title}</strong></p></div>
                                 <div className='vote'><strong>Precio:</strong> {movie.vote_average} €</div>
                                 <div className="rentButtonBox" id="rentButtonBox">
-
+    
                                     <Button
                                         style={{
                                             border: "none",
@@ -86,7 +83,7 @@ export default function MostrarCarrousel({ endpoint }) {
                                             borderRadius: "5px"
                                         }}>
                                         Alquilar
-                                </Button>
+                                    </Button>
                                     <Button
                                         style={{
                                             border: "none",
@@ -96,15 +93,15 @@ export default function MostrarCarrousel({ endpoint }) {
                                             borderRadius: "5px"
                                         }} onClick={() => { setShowModal(true); setShowMovieActual(movie) }}>
                                         +info
-                                    </Button>
-
+                                        </Button>
+    
                                 </div>
                             </div>
                         </div>
                     </div>
                     </div>
                 })}
-
+    
             </Slider>
             <Modal
                 visible={showModal}
@@ -128,8 +125,8 @@ export default function MostrarCarrousel({ endpoint }) {
                 <p>{showMovieActual.overview}</p>
                 <p>Día de salida: <br /> {dayjs(showMovieActual.release_date).format('DD-MM-YYYY')}</p>
                 <p>Precio: <br /> {showMovieActual.vote_average} €</p>
-
-
+    
+    
                 <div className="footerMovie">
                     <Button
                         style={{
@@ -139,8 +136,8 @@ export default function MostrarCarrousel({ endpoint }) {
                             marginRight: "1em",
                             borderRadius: "5px"
                         }}>Alquilar
-                        </Button>
-
+                            </Button>
+    
                     <Button
                         onClick={() => setShowModal(false)}
                         style={{
@@ -149,13 +146,16 @@ export default function MostrarCarrousel({ endpoint }) {
                             color: "white",
                             borderRadius: "5px"
                         }}>Cerrar
-                        </Button>
+                            </Button>
                 </div>
-
+    
             </Modal>
         </>
     )
+    
+    }
+    
 
-}
+
 
 
